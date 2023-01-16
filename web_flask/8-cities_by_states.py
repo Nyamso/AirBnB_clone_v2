7493e4e81667206d8c414c284ba94badbc29b12d
Nyamso <msosarehema@outlook.com>
2023-01-16 13:03:20 +0000

    Corrections of tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """Starts a Flask web application for displaying cities of a state"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
-from models import storage, State
-from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
